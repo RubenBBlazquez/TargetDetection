bb2906db523abf5c1f3edd4db0c6f7c2ecade669
rubenbblazquez <rubenbarroso16@gmail.com>
2023-06-10 15:24:18 +0200

    []: add new servo module to control it with raspberri

---
+++ RaspberriModules/DataClasses/ServoModule.py
@@ -0,0 +1,48 @@
+from time import sleep
+from attrs import define, field
+import RPi.GPIO as GPIO
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        
+        return cls._instances[cls]
+
+
+class ServoGPIOModule(metaclass=SingletonMeta):
+    def __init__(self, gpin: int) -> None:
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(gpin, GPIO.OUT)
+        self.servo = GPIO.PWM(gpin, 50)
+        self.servo.start(0)
+
+
+@define(auto_attribs=True)
+class ServoMovement:
+    gpin: int = field(default=11)
+    position: int = field(default=0)
+    servo: GPIO.PWM = field(default=None)
+    
+    def __attrs_post_init__(self):
+        self.servo = ServoGPIOModule(self.gpin).servo
+        self.default_move()            
+
+    def _calculate_duty_cycle_percentage(self, angle: int):
+        return (angle / 18) + 3
+
+    def default_move(self):
+        self.servo.ChangeDutyCycle(self.position)
+        sleep(0.5)
+
+
+    def move_to(self, position):
+        self.servo.ChangeDutyCycle(position)
+        sleep(0.5)
+
+    def stop(self):
+        self.servo.ChangeDutyCycle(0)
--- RaspberriModules/yolo_detection_real_time.py
+++ RaspberriModules/yolo_detection_real_time.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from pprint import pprint
+from time import sleep
 from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,7 +9,7 @@ from picamera2 import MappedArray, Picamera2, Preview
 import torch
 from torch import tensor
 import pandas as pd
-from DataClasses.ServoModule import ServoModule
+from DataClasses.ServoModule import ServoMovement
 
 normalSize = (400, 500)
 
@@ -35,32 +36,44 @@ def get_valid_predictions(predictions: pd.DataFrame) -> List[tensor]:
     return valid_predictions.astype(np.int32)
 
 def predict_last_images(last_actions: List[np.array]):
-    for action in last_actions[::-1]:
+    for index, action in enumerate(last_actions[::-1]):
         image, servo_module = action
         results = model(image)
-        predictions = results.pandas().xywh
+        predictions = results.pandas().xywh[0]
         valid_predictions = get_valid_predictions(predictions)
 
-        if valid_predictions.size == 0:
-            servo_module.move()
+        print(index, valid_predictions)
+        if valid_predictions.size > 0:
+            servo_module.default_move()
+            break
 
 
 frames = 0
 last_actions = []
 angle = 0
-gpin_horizontal_servo = 11
+gpin_horizontal_servo = 13
+increment = 2.5
 
 while True:
     frames += 1
     image = picam2.capture_array()
     
     if frames >= 15:
-        last_actions.append((image, ServoModule(gpin_horizontal_servo, angle)))
-        
-        if len(last_actions) == 5:
+        print('---- add one more movement ----')
+        servo = ServoMovement(gpin_horizontal_servo, angle)
+        last_actions.append((image, servo))
+
+        if len(last_actions) == 4:
+            servo.stop()
             predict_last_images(last_actions)
+            last_actions = []
+            increment = -increment
+        
+        angle += increment
+
+        if angle < 0:
+            angle = 0
 
         frames = 0
-        angle += 30
 
 
