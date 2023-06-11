92c1a6081984e8d93ee471d6462b4693400b4358
rubenbblazquez <rubenbarroso16@gmail.com>
2023-06-11 11:28:33 +0200

    []: remove unused yolo weights on root project

--- Predictions/CeleryTasks.py
+++ Predictions/CeleryTasks.py
@@ -1,10 +1,13 @@
 from celery import app
 from Predictions.models import RawPredictionsData
 import logging
+from Core.Services.TargetDetection.YoloTargetDetection import YoloTargetDetection
 
 @app.shared_task
 def check_prediction(*args):
     raw_data = RawPredictionsData(*args)
     logging.info('----------------------------------------------------------------------')
     logging.info(f'TASL RECIVED: yolo will predict with raw predictions data {raw_data}')
+    yoloModel = YoloTargetDetection()
+
 
