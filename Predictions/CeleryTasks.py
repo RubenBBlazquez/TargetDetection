c862df2f6a518c68b29e8b93fec92d7a923f3810
rubenbblazquez <rubenbarroso16@gmail.com>
2023-06-12 22:35:14 +0200

    []: modify celery tasks

--- Predictions/CeleryTasks.py
+++ Predictions/CeleryTasks.py
@@ -7,6 +7,7 @@ import pickle
 import os
 import numpy as np
 import pandas as pd
+import cv2
 
 @app.shared_task
 def check_prediction(*args):
@@ -14,18 +15,22 @@ def check_prediction(*args):
     image = pickle.loads(image_bytes)
 
     logging.info('----------------------------------------------------------------------')
-    logging.info(f'TASK RECEIVED: {datetime.now()}')
+    logging.info(f'TASK (check_prediction) RECEIVED: {datetime.now()}')
 
     if datetime.now().day != date.day:
-        logging.info(f'TASK REJECTED: DATE DAY ({date.day}) ARGUMENT IS DIFFERENT FROM ACTUAL DATE ({datetime.now().day})')
+        logging.info(f'TASK (check_prediction) REJECTED: DATE DAY ({date.day}) ARGUMENT IS DIFFERENT FROM ACTUAL DATE ({datetime.now().day})')
 
         return
     
     model = YoloTargetDetection(os.getenv('YOLO_MODEL_NAME'))
     result_prediction = model.predict(image)
     predicted_labels = result_prediction.pandas().xywh[0]
-    predicted_labels = predicted_labels[predicted_labels['confidence'] > 60]
+    
+    logging.info(predicted_labels['confidence'])
+    logging.info(predicted_labels['confidence'] > 0.60)
 
+    predicted_labels = predicted_labels[predicted_labels['confidence'] > 0.60]
+    
     logging.info(f'Predicted Labels: \n {predicted_labels}')
 
     if not predicted_labels.empty:
@@ -33,13 +38,11 @@ def check_prediction(*args):
             lambda labels: 
                 launch_prediction_action.apply_async(
                     RawPredictionData(pickle.dumps(image), pickle.dumps(labels), servo_position, datetime.now()),
-                    ignore_result=True
+                    ignore_result=True,
+                    priority=10
                 ), 
             axis=1
         )
-
-    logging.info('----------------------------------------------------------------------')
-
 
 @app.shared_task
 def launch_prediction_action(*args):
@@ -48,10 +51,20 @@ def launch_prediction_action(*args):
     labels: pd.Series = pickle.loads(labels_bytes)
 
     if datetime.now().day != date.day:
-        logging.info(f'TASK REJECTED: DATE DAY ({date.day}) ARGUMENT IS DIFFERENT FROM ACTUAL DATE ({datetime.now().day})')
+        logging.info(f'TASK (launch_prediction_action) REJECTED: DATE DAY ({date.day}) ARGUMENT IS DIFFERENT FROM ACTUAL DATE ({datetime.now().day})')
 
         return
 
+    image = cv2.rectangle(
+        image, 
+        (labels.xcenter, labels.ycenter), 
+        (labels.xcenter + labels.width, labels.ycenter + labels.width),
+        (36,255,12), 
+        1
+    )
+
+    cv2.imshow(image)
+        
     #os.system('python -m celery -A Core purge -f')
 
 
