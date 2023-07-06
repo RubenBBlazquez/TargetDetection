d4dfe10b1f8f7a49296ae80e451e3a846bd8f47f
rbblazquez <rubenbarroso16@gmail.com>
2023-07-06 22:31:02 +0200

    []: New Class DistanceCalculations to calculate distances inside an image based on pixels

--- Predictions/services/DistanceCalculations.py
+++ Predictions/services/DistanceCalculations.py
@@ -1,8 +1,47 @@
+import logging
+from typing import Tuple, List, NamedTuple
+
+import attr
+import cv2
 import numpy as np
 import pandas as pd
 from attr import define
 
-CM_IN_PIXELS = 37.8
+CM_IN_PIXELS = 37.7952755906
+
+
+class Point(NamedTuple):
+    """
+        This class is used to represent a point in a line.
+
+        Attributes:
+        -----------
+        x: int
+            This attribute contains the x coordinate of the point.
+        y: int
+            This attribute contains the y coordinate of the point.
+    """
+    x: int
+    y: int
+
+
+@define(auto_attribs=True)
+class Line:
+    """
+        This class is used to represent a line.
+
+        Attributes:
+        -----------
+        pt1: Point
+            This attribute contains the first point of the line.
+        pt2: Point
+            This attribute contains the second point of the line.
+        axis: int
+            This attribute contains the axis of the line.
+    """
+    pt1: Point
+    pt2: Point
+    axis: int = attr.ib(default=0)
 
 
 @define(auto_attribs=True)
@@ -12,20 +51,22 @@ class DistanceCalculations:
 
         Attributes:
         -----------
-        distance: float
-            This attribute contains the distance in meters.
-        angle: float
-            This attribute contains the angle in degrees.
-        servo_position: int
-            This attribute contains the servo position.
-        date: datetime
-            This attribute contains the date when the calculation was made.
+        image: np.array
+            This attribute contains the image.
+        x0: int
+            This attribute contains the x0 coordinate of the labels.
+        y0: int
+            This attribute contains the y0 coordinate of the labels.
+        x1: int
+            This attribute contains the x1 coordinate of the labels.
+        y1: int
+            This attribute contains the y1 coordinate of the labels.
     """
     image: np.array
-    x0: float
-    y0: float
-    x1: float
-    y1: float
+    x0: int
+    y0: int
+    x1: int
+    y1: int
 
     @classmethod
     def create_from(cls, image: np.array, labels: pd.Series):
@@ -44,10 +85,26 @@ class DistanceCalculations:
         x1 = labels.xcenter + labels.width / 2
         y1 = labels.ycenter + labels.height / 2
 
-        return cls(image, x0, y0, x1, y1)
+        return cls(image, int(x0), int(y0), int(x1), int(y1))
 
-    @property
-    def distance_to_left(self) -> float:
+    def get_distance_in_cm(self, line: Line, axis=0) -> float:
+        """
+        This method is used to calculate the distance in cm.
+
+        Parameters
+        ----------
+        axis:
+          define if we want to calculate distance in x-axis or y-axis
+        line: Line
+            define the line to calculate the distance
+        """
+
+        if axis == 0:
+            return abs(line.pt2.x - line.pt1.x) / CM_IN_PIXELS
+
+        return abs(line.pt2.y - line.pt1.y) / CM_IN_PIXELS
+
+    def distance_to_left(self) -> Line:
         """
             This property is used to calculate the distance to the left side of the image.
 
@@ -56,7 +113,44 @@ class DistanceCalculations:
             float
                 This method returns the distance to the left side of the image in meters.
         """
-        return self.x0 / CM_IN_PIXELS
+        return Line(
+            Point(0, int(self.y0)),
+            Point(int(self.x0), int(self.y0))
+        )
 
-    def draw_lines_into_image(self, lines: list) -> None:
-        cv2.dra
+    def draw_lines_into_image(self, lines: List[Line]) -> None:
+        """
+        This method is used to draw lines into the image.
+
+        Parameters
+        ----------
+        lines: List[Line]
+            This parameter contains the lines to be drawn into the image.
+        """
+        cv2.rectangle(
+            self.image,
+            (self.x0, self.y0),
+            (self.x1, self.y1),
+            (36, 255, 12),
+            1
+        )
+
+        for line in lines:
+            logging.error(f"line: {({line.pt2.x // 2}, {line.pt2.y - 10})}")
+            cv2.line(self.image, line.pt1, line.pt2, (255, 0, 0), 1)
+            cv2.putText(
+                self.image,
+                f'{round(self.get_distance_in_cm(line), 3)} cm',
+                (line.pt2.x // 2, line.pt2.y - 10),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.5,
+                color=(255, 0, 0),
+                thickness=1,
+                lineType=cv2.LINE_AA
+            )
+
+        cv2.imshow('lines', self.image)
+        cv2.waitKey(3000)
+        cv2.destroyAllWindows()
+
+        return self.image
---
+++ Predictions/tests/test_service_distance_calculations.py
@@ -0,0 +1,24 @@
+import logging
+
+import cv2
+import numpy as np
+import pandas as pd
+
+from Predictions.services.DistanceCalculations import DistanceCalculations
+
+
+def test_calculate_left_distance():
+    image = np.array(cv2.imread('Predictions/tests/assets/target_image.jpg'))
+    labels = pd.Series({
+        'confidence': 0.99,
+        'height': 100,
+        'width': 100,
+        'xcenter':100,
+        'ycenter': 100
+    })
+    distance_calculations = DistanceCalculations.create_from(image, labels)
+    line_to_left = distance_calculations.distance_to_left()
+    distance_calculations.draw_lines_into_image([line_to_left])
+    logging.error(f"distance_calculations.distance_to_left: {line_to_left}")
+
+    assert False
