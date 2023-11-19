75455445f369e02aa358caae28c8c81e9f05475e
rbblazquez <rubenbarroso16@gmail.com>
2023-11-19 16:48:59 +0100

    trying reinforcing learning

--- BackEndApps/Predictions/management/commands/real_time_detection.py
+++ BackEndApps/Predictions/management/commands/real_time_detection.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 from django.core.management.base import BaseCommand
+
+from BackEndApps.Predictions.ml_models import ServoMotorEnv, train_policy_network
 from BackEndApps.Predictions.models import RawData
 import pickle
 from RaspberriModules.DataClasses.CustomPicamera import CustomPicamera
@@ -10,23 +12,10 @@ from datetime import datetime
 import time
 from enum import Enum
 import numpy as np
-
+import tensorflow as tf
 class CameraType(Enum):
     USB = 'usb'
     CSI = 'csi'
-def enumerate_cameras():
-    index = 0
-    arr = []
-    while True:
-        cap = cv2.VideoCapture(index)
-        if not cap.read()[0]:
-            break
-        else:
-            arr.append(index)
-        cap.release()
-        index += 1
-    return arr
-
 
 class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
@@ -51,50 +40,60 @@ class Command(BaseCommand):
         return image
     
     def handle(self, *args, **options):
-        frames = 0
-        angle = 1
-        gpin_horizontal_servo = int(os.getenv('X_SERVO_PIN'))
-        increment = 1
-        servo_movements = 0
-        cv2.startWindowThread()
-        servo = ServoMovement(gpin_horizontal_servo, angle, name='x1')
+        # frames = 0
+        # angle = 1
+        # gpin_horizontal_servo = int(os.getenv('X_SERVO_PIN'))
+        # increment = 1
+        # servo_movements = 0
+        # cv2.startWindowThread()
+        # servo = ServoMovement(gpin_horizontal_servo, angle, name='x1')
+        #
+        # while True:
+        #     frames += 1
+        #     image = self.get_image_from_camera(CameraType.CSI, True)
+        #     cv2.imshow("CSI Camera", image)
+        #     cv2.waitKey(1)
+        #     is_shoot_in_progress = os.path.exists('RaspberriModules/assets/shoot_in_progress.tmp')
+        #
+        #     if frames < 15:
+        #         continue
+        #
+        #     if is_shoot_in_progress:
+        #         servo.stop()
+        #         continue
+        #
+        #     if angle < 0:
+        #         angle = 1
+        #
+        #     servo = ServoMovement(gpin_horizontal_servo, angle, name='x1')
+        #     servo.move_to(angle)
+        #     time.sleep(0.4)
+        #
+        #     servo_movements += 1
+        #     raw_data = RawData(image=pickle.dumps(image), servo_position=angle, date=datetime.now())
+        #
+        #     check_prediction.apply_async(raw_data, queue='check_predictions', ignore_result=True, prority=1)
+        #
+        #     if servo_movements % 10 == 0:
+        #         increment = -increment
+        #
+        #     angle += increment
+        #     # we check if servo do all possible movements and clean unnecessary tasks
+        #     if servo_movements % 20 == 0:
+        #         print('purging tasks')
+        #         servo_movements = 0
+        #         purge_celery.apply_async(('check_predictions', ), queue='purge_data', ignore_result=True, prority=1)
+        #
+        #     frames = 0
+        #
+        # cv2.destroyAllWindows()
 
-        while True:
-            frames += 1
-            image = self.get_image_from_camera(CameraType.CSI, True)
-            cv2.imshow("CSI Camera", image)
-            cv2.waitKey(1)       
-            is_shoot_in_progress = os.path.exists('RaspberriModules/assets/shoot_in_progress.tmp')
-
-            if frames < 15:
-                continue
-
-            if is_shoot_in_progress:
-                servo.stop()
-                continue
-            
-            if angle < 0:
-                angle = 1
-            
-            servo = ServoMovement(gpin_horizontal_servo, angle, name='x1')
-            servo.move_to(angle)
-            time.sleep(0.4)
-
-            servo_movements += 1
-            raw_data = RawData(image=pickle.dumps(image), servo_position=angle, date=datetime.now())
-            
-            check_prediction.apply_async(raw_data, queue='check_predictions', ignore_result=True, prority=1)
-
-            if servo_movements % 10 == 0:
-                increment = -increment
-                
-            angle += increment
-            # we check if servo do all possible movements and clean unnecessary tasks
-            if servo_movements % 20 == 0:
-                print('purging tasks')
-                servo_movements = 0
-                purge_celery.apply_async(('check_predictions', ), queue='purge_data', ignore_result=True, prority=1)
-
-            frames = 0
-
-        cv2.destroyAllWindows()
+        env = ServoMotorEnv()
+        policy_net = tf.keras.Sequential(
+            [
+                tf.keras.layers.Dense(32, activation="relu"),
+                tf.keras.layers.Dense(1, activation="sigmoid"),
+            ]
+        )
+        train_policy_network(env, policy_net, tf.optimizers.Adam(learning_rate=0.01))
+        policy_net.save('policy_net.h5')
